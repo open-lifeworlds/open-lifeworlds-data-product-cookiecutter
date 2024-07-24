63be5a085f85368ab7b72bfe9c63bdf7c43f604c
Florian Schwanz <florian.schwanz@interoberlin.de>
2024-07-24 10:21:40 +0200

    fix: typo

--- open-lifeworlds-data-product-{{cookiecutter.data_product_id}}/lib/load/data_loader.py
+++ open-lifeworlds-data-product-{{cookiecutter.data_product_id}}/lib/load/data_loader.py
@@ -11,31 +11,23 @@ def load_data(source_path, results_path, clean=False, quiet=False):
     for subdir, dirs, files in sorted(os.walk(source_path)):
         for file_name in sorted(files):
             _, file_extension = os.path.splitext(file_name)
+
             subdir = subdir.replace(f"{source_path}/", "")
 
-            if (("berlin-15-minute-city" in subdir
-                 or "berlin-lor-geodata-geojson" in subdir)
-                    and file_extension in [".geojson"]
-                    and "metrics" not in file_name
-            ):
-                try:
-                    year = re.findall(r"\b\d{4}\b", file_name)[-1]
-                    month = re.findall(r"\b\d{2}\b", file_name)[-1]
-                    results_subdir = f"{subdir}-{year}-{month}"
-                except Exception as _:
-                    results_subdir = subdir
+            year = re.findall(r"\b\d{4}\b", file_name)[-1]
+            month = re.findall(r"\b\d{2}\b", file_name)[-1]
 
-                # Make results path
-                os.makedirs(os.path.join(results_path, results_subdir), exist_ok=True)
+            # Make results path
+            os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
 
-                source_file_path = os.path.join(source_path, subdir, file_name)
-                results_file_path = os.path.join(results_path, results_subdir, file_name)
+            source_file_path = os.path.join(source_path, subdir, file_name)
+            results_file_path = os.path.join(results_path, subdir, file_name)
 
-                # Check if file needs to be copied
-                if clean or not os.path.exists(results_file_path):
-                    shutil.copyfile(source_file_path, results_file_path)
+            # Check if file needs to be loaded
+            if clean or not os.path.exists(results_file_path):
+                shutil.copyfile(source_file_path, results_file_path)
 
-                    if not quiet:
-                        print(f"✓ Load {file_name}")
-                else:
-                    print(f"✓ Already exists {subdir}/{file_name}")
+                if not quiet:
+                    print(f"✓ Load {file_name}")
+            else:
+                print(f"✓ Already exists {subdir}/{file_name}")
